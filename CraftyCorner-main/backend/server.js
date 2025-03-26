ec4a21683e9f133ae61016eef61515187a272f4d
inu1193 <inayath2410@gmail.com>
2025-03-26 16:13:32 +0530

    Cors update

--- CraftyCorner-main/backend/server.js
+++ CraftyCorner-main/backend/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const connectDatabase = require("./db/Database");
 const cloudinary = require("cloudinary");
-
+const cors = require("cors");
 // Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
