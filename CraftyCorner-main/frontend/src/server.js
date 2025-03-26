0e2d12ad26064223da9f1c7f3856dd6d9ba76ea2
inu1193 <inayath2410@gmail.com>
2025-03-26 15:55:25 +0530

    Render URL

--- CraftyCorner-main/frontend/src/server.js
+++ CraftyCorner-main/frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://craftycorner-j942.onrender.com/api/v2";
 
 
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://craftycorner-j942.onrender.com/"
