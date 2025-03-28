5c8e34940e355f7fdb3504735df7096c7daa6509
inu1193 <inayath2410@gmail.com>
2025-03-28 11:08:44 +0530

    Base URL  reupdate

--- CraftyCorner-main/frontend/src/server.js
+++ CraftyCorner-main/frontend/src/server.js
@@ -1,4 +1,4 @@
 export const server = "https://craftycorner-j942.onrender.com/api/v2";
 
 
-export const backend_url = "https://craftycorner-j942.onrender.com/"
+export const backend_url = "https://craftycorner-j942.onrender.com"
