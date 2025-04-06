43406d3a1c7b95b2b11b79f0cf654b79ad6db0cb
Inayath S <98113774+inu1193@users.noreply.github.com>
2025-04-06 17:28:46 +0530

    Update activation

--- CraftyCorner-main/backend/controller/shop.js
+++ CraftyCorner-main/backend/controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://ccshop.netlify.app/seller/activation/${activationToken}`;
+    const activationUrl = `https://craftycorner-j942.onrender.com/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
